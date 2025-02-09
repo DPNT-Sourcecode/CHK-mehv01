174079982c3f522f52a11dcff5d5ae97a340d7f6
sbx_user1051 <sbx_user1051@169.254.4.61>
2025-02-09 22:50:27 +0000

    Sun Feb 09 22:50:27 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,7 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum2(self):
+        assert sum_solution.compute(3, 4) == 7
+
