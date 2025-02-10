9f1dfdd3b789d3d651a70fd2fc3ad20aa8dbf1ec
sbx_user1051 <sbx_user1051@169.254.5.189>
2025-02-10 03:32:33 +0000

    Mon Feb 10 03:32:33 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,7 +15,7 @@ def checkout(skus: str) -> int:
         'H': 10,
         'I': 35,
         'J': 60,
-        'K': 80,
+        'K': 70,
         'L': 90,
         'M': 15,
         'N': 40,
@@ -23,14 +23,14 @@ def checkout(skus: str) -> int:
         'P': 50,
         'Q': 30,
         'R': 50,
-        'S': 30,
+        'S': 20,
         'T': 20,
         'U': 40,
         'V': 50,
         'W': 20,
-        'X': 90,
-        'Y': 10,
-        'Z': 50
+        'X': 17,
+        'Y': 20,
+        'Z': 21
     }
 
     #Special bulk discounts
@@ -87,3 +87,4 @@ def checkout(skus: str) -> int:
 
     return total_price #return final checkout total
     
+
