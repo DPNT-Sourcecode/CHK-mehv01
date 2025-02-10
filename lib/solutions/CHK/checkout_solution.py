e64f7990ed354d05ba25c23f0568c428a24d5710
sbx_user1051 <sbx_user1051@169.254.5.189>
2025-02-10 02:24:20 +0000

    Mon Feb 10 02:24:20 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,32 @@
-
+from collections import Counter
+item_counts = Counter(skus)
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    #dict for price table
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    #dict for offers: item -> (num required, discounted price)
+    special_offers = {
+        'A': (3, 130),
+        'B': (2, 45)
+    }
+
+    for c in skus:
+        if c not in prices:
+            return -1
+    
+    total_price = 0
+
+    for item, count in item_counts.items():
+        print(item, count)
+        # if item in special_offers:
+            
+    
+
